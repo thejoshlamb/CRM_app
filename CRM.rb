0b90f0ed92a9fca949e94d96d0d59992bbdbaf2e
thejoshlamb <joshlamb@gmail.com>
2013-10-25 16:31:03 -0400

    rolodex and CRM refactored

--- CRM.rb
+++ CRM.rb
@@ -4,7 +4,7 @@ require_relative "rolodex"
 class CRM
 
   def initialize
-    run
+    @rolodex = Rolodex.new
   end
 
   def run
@@ -15,6 +15,26 @@ class CRM
 
   def clear_screen
     puts "\e[H\e[2J"
+  end
+
+  def warn_if_empty
+    puts "No contacts yet!" unless @rolodex.contacts.any?
+  end
+
+  def pause_for_user
+    puts "Press Enter to continue"
+    wait_for_enter = gets
+  end
+
+  def confirm_choice
+    puts "Is this correct? (Y/N)"
+    confirm_choice = gets.chomp
+    case confirm_choice[0]
+    when "y" || "Y"
+      return true
+    else
+      return false
+    end
   end
 
   def main_menu
@@ -75,39 +95,80 @@ class CRM
     puts "Adding #{first_name} #{last_name}"
     puts "Email address: #{email}"
     puts "Notes: #{note}"
-    puts "Is this correct? (Y/N)"
-    confirm_choice = gets.chomp
-    case confirm_choice[0]
-    when "y" || "Y"
+    case confirm_choice
+    when true
       contact = Contact.new(first_name, last_name, email, note)
-      Rolodex.add_contact(contact)
+      @rolodex.add(contact)
     else
       add_new_contact
     end
   end
 
   def modify_existing_contact
-    puts "Enter the ID of the contact you would like to edit:"
-    id_to_modify = gets.chomp.to_i
-    Rolodex.modify_contact
+    clear_screen
+    contact_exists = false
+    while contact_exists = false do
+      puts "Enter the ID of the contact you would like to edit:"
+      id_to_modify = gets.chomp.to_i
+      contact_exists = true if contact_exists?(id_to_modify)
+      puts "That ID does not exist"
+    end
+    clear_screen
+    puts "Which Attribute would you like to modify?"
+    puts "[1] ID"
+    puts "[2] Name"
+    puts "[3] Email"
+    puts "[4] Notes"
+    attribute_choice = gets.to_i
+    @rolodex.modify_contact(id_to_modify,)
   end
 
   def delete_contact
+    puts "Enter the ID of the contact you would like to delete:"
+    
   end
 
   def display_contact
     clear_screen
     puts "Enter the id of the Contact you would like to display:"
     user_choice = gets.to_i
-    Rolodex.display_particular_contact(user_choice)
+    @rolodex.display_particular_contact(user_choice)
+    pause_for_user
   end
 
   def display_all_contacts
-    Rolodex.display_all_contacts
+    clear_screen
+    warn_if_empty
+    @rolodex.display_all_contacts
+    pause_for_user
   end
 
   def display_attribute
-    Rolodex.display_all_attributes
+    clear_screen
+    warn_if_empty
+    puts "Which attribute?"
+    puts "[1] ID"
+    puts "[2] Name (Last, First)"
+    puts "[3] Name (First, Last)"
+    puts "[4] Email"
+    puts "[5] Notes"
+    user_choice = gets.to_i
+    clear_screen
+    case user_choice
+    when 1
+      @rolodex.contacts.each {|contact| puts contact.id}
+    when 2
+      @rolodex.contacts.each {|contact| puts "#{contact.last_name}, #{contact.first_name}" }
+    when 3
+      @rolodex.contacts.each {|contact| puts "#{contact.first_name}, #{contact.last_name}" }
+    when 4
+      @rolodex.contacts.each {|contact| puts contact.email }
+    when 5
+      @rolodex.contacts.each {|contact| puts contact.notes }
+    else
+      raise
+    end
+    pause_for_user
   end
 
   def main_menu_error
@@ -121,6 +182,17 @@ class CRM
   def quit
     exit
   end
+
+  def loadpresets
+    contact = Contact.new("Bruce", "Wayne", "bwanye@batmanor.com", "He's Batman")
+    @rolodex.add(contact)
+    contact = Contact.new("Some", "Guy", "whatever@whoever.com", "just some guy")
+    @rolodex.add(contact)
+    contact = Contact.new("Alfred", "Hitchcock", "Hitch@film.com", "A fat guy")
+    @rolodex.add(contact)
+  end
 end
 
-greatest_crm_ever = CRM.new
+newcrm = CRM.new
+newcrm.loadpresets
+newcrm.run
--- rolodex.rb
+++ rolodex.rb
@@ -1,80 +1,40 @@
 class Rolodex
   attr_accessor :id, :contacts
 
-  @@contacts = []
-  @@id = 1000
-
-
-  def self.pause_for_user
-    puts "Press Enter to continue"
-    wait_for_enter = gets
+  def initialize
+    @contacts = Array.new
+    @id = 1000
   end
 
-  def self.add_contact(contact)
-    contact.id = @@id
-    @@contacts << contact
-    @@id += 1
+  def add(contact)
+    contact.id = @id
+    @contacts << contact
+    @id += 1
   end
 
-  def self.clear_screen # i don't understand why this needs to be a .self
-    puts "\e[H\e[2J"
-  end
-
-  def self.warn_if_empty
-    puts "No contacts yet!" unless contacts.any?
-  end
-
-  def self.display_all_contacts
-    clear_screen
-    warn_if_empty
-    contacts.each { |contact| contact.display }
-    pause_for_user
-  end
-
-  def self.display_all_attributes
-    clear_screen
-    warn_if_empty
-    puts "Which attribute?"
-    puts "[1] ID"
-    puts "[2] Name (Last, First)"
-    puts "[3] Name (First, Last)"
-    puts "[4] Email"
-    puts "[5] Notes"
-    user_choice = gets.to_i
-    case user_choice
-    when 1
-      clear_screen
-      contacts.each {|contact| puts contact.id}
-      pause_for_user
-    when 2
-      clear_screen
-      contacts.each {|contact| puts "#{contact.last_name}, #{contact.first_name}" }
-      pause_for_user
-    when 3
-      clear_screen
-      contacts.each {|contact| puts "#{contact.first_name}, #{contact.last_name}" }
-      pause_for_user
-    when 4
-      clear_screen
-      contacts.each {|contact| puts contact.email }
-      pause_for_user
-    when 5
-      clear_screen
-      contacts.each {|contact| puts contact.notes }
-      pause_for_user
-    else
-      puts "NOPENOPENOPENOPE"
+  def contact_exists?(id)
+    @contacts.each do |contact|
+      return true if contact.id == id
+      return false
     end
   end
 
-    def self.display_particular_contact(desired_id)
-      contacts.each { |contact| contact.display if contact.id == desired_id }
-    end
-
-    def self.modify_contact
-    end
-
-    def self.delete_contact
-    end
+  def display_all_contacts
+    @contacts.each {|contact| contact.display}
   end
+
+  #def display_info_by_attribute(desired_attribute)
+  #end
+
+  def display_particular_contact(desired_id)
+     contacts.each { |contact| contact.display if contact.id == desired_id }
+  end
+
+  def modify_contact(desired_id,attribute_to_edit)
+
+  end
+
+  def delete_contact
+  end
+
 end
